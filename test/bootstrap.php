0ee07339b9c72b499c13d805369ed240a21c99b2
Sam-Mauris Yong <mauris@hotmail.sg>
2012-07-21 10:20:54 +0800

    updated include path

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -5,5 +5,7 @@
  */
 use Packfire\Octurlpus\Bootstrap;
 
+set_include_path(__DIR__
+        . PATH_SEPARATOR . get_include_path());
 include(__DIR__ . '/../src/Packfire/Octurlpus/Bootstrap.php');
 Bootstrap::initialize();
