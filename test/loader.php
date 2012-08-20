3193c156433fbbc1d3f87438272c346196719159
Sam-Mauris Yong <mauris@hotmail.sg>
2012-08-20 08:50:18 +0800

    autoload test bootstrap fix

--- test/loader.php
+++ test/loader.php
@@ -1,6 +1,6 @@
 <?php
 
 call_user_func(function(){
-    $loader = require __DIR__ . '/../vendors/autoload.php';
+    $loader = require __DIR__ . '/../vendor/autoload.php';
     $loader->add(null, __DIR__);
 });
