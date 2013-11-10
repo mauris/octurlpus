51ba5b79fb2e04ef1388c1beea6f7e5956e61574
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-10 15:50:07 +0800

    standardization updates

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,21 +1,4 @@
 <?php
 
-/**
- * @author Sam-Mauris Yong / mauris@hotmail.sg
- */
-set_include_path(realpath(__DIR__ . '/../src/')
-        . PATH_SEPARATOR . get_include_path());
-spl_autoload_register(function($class) {
-    $class = ltrim($class, '\\');
-    $fileName  = '';
-    $namespace = '';
-    $lastNsPos = strrpos($class, '\\');
-    if($lastNsPos){
-        $namespace = substr($class, 0, $lastNsPos);
-        $class = substr($class, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace)
-                . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
-    require $fileName;
-});
+$loader = require(__DIR__ . '/../vendor/autoload.php');
+$loader->add('Packfire\\Octurlpus\\', __DIR__);
